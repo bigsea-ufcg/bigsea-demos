9f8a4f0c1133dbb5adbb1437b4399a86ee45639a
Roberto Nascimento Jr <robertonscjr@gmail.com>
2018-02-21 12:35:10 -0300

    Script updated

--- final/run.sh
+++ final/run.sh
@@ -4,23 +4,23 @@ echo   "=== BIGSEA WP3 DEMO ==="
 echo ""
 echo   "======== BEGIN ========"
 echo "-> Cleaning existing files, please wait"
-ssh -i ~/.ssh/bigsea ubuntu@10.11.4.225 "source /home/ubuntu/clean.sh" &> /dev/null
+ssh -i ~/.ssh/bigsea ubuntu@10.11.4.225 "source /home/ubuntu/clean_bulma.sh" &> /dev/null
 
 echo ""
 echo "-> Running applications"
 printf "1. BULMA\n"
 python client.py config/"$1"/bulma.cfg
 
-printf "\n2. BUSTE\n"
-python client.py config/"$1"/buste.cfg
+#printf "\n2. BUSTE\n"
+#python client.py config/"$1"/buste.cfg
 
-printf "\n3. OD matrix builder\n"
-python client.py config/"$1"/od.cfg
+#printf "\n3. OD matrix builder\n"
+#python client.py config/"$1"/od.cfg
 
-printf "\n4. Pre-processing\n"
-python client.py config/"$1"/preproc.cfg
+#printf "\n4. Pre-processing\n"
+#python client.py config/"$1"/preproc.cfg
 
-printf "\n5. Training\n"
-python client.py config/"$1"/train.cfg
+#printf "\n5. Training\n"
+#python client.py config/"$1"/train.cfg
 
 echo "========= END ========="
--- final/run_experiments.sh
+++ final/run_experiments.sh
@@ -5,16 +5,16 @@ echo ""
 echo "======== BEGIN ========"
 
 echo "--> Running applications"
-echo "Experiments:"
-echo "1. starting_cap: 50 - expected_time: 15min - actuation_size: 25% - min: 50% - max: 100%"
-echo "2. starting_cap: 75 - expected_time: 15min - actuation_size: 25% - min: 50% - max: 100%"
-echo "3. starting_cap: 50 - expected_time: 15min - actuation_size: 50% - min: 50% - max: 100%"
-for i in `seq 1 7`;
+for i in `seq 0 2`;
 do
-    echo "Experiment $i"
-    ssh -i ~/.ssh/bigsea ubuntu@10.11.4.225 "source /home/ubuntu/clean_bulma.sh" &> /dev/null
-    python client.py config/demo/"$i".cfg
-    echo ""
+    ssh -i ~/.ssh/bigsea ubuntu@10.11.4.225 "source /home/ubuntu/clean.sh" &> /dev/null
+
+    for j in `seq 1 5`;
+    do
+        exp_number=$(($i*5+$j))
+        echo "Experiment $exp_number"
+        python client.py config/all/"$exp_number".cfg
+    done
 done
 
 echo "========= END ========="
