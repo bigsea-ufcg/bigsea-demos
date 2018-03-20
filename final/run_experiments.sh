d0d8cc30b8856c78f883fd7e5fd076ea731c0fa7
Roberto Nascimento Jr <robertonscjr@gmail.com>
2018-03-20 17:03:31 -0300

    Update demo script

--- final/run_experiments.sh
+++ final/run_experiments.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "=== BIGSEA WP3 DEMO ==="
+echo "--- BIGSEA WP3 CLIENT ---"
 echo ""
-echo "======== BEGIN ========"
+echo "========= BEGIN ========="
 
-echo "--> Running applications"
-for i in `seq 0 2`;
+echo "1. Cleaning old files"
+for i in `seq 0 0`;
 do
     ssh -i ~/.ssh/bigsea ubuntu@10.11.4.225 "source /home/ubuntu/clean.sh" &> /dev/null
 
-    for j in `seq 1 5`;
+echo "2. Running BULMA"
+    for j in `seq 1 1`;
     do
-        exp_number=$(($i*5+$j))
-        echo "Experiment $exp_number"
+        exp_number=$j
+#        echo "Experiment $exp_number"
         python client.py config/all/"$exp_number".cfg
     done
 done
 
-echo "========= END ========="
+echo "========== END =========="
